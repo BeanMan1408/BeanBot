6fb7cbde2277685ed641c74dba368723aa217558
BeanMan1408 <86612985+BeanMan1408@users.noreply.github.com>
2021-08-11 22:14:53 -0400

    Add files via upload
    NOTE: BotPanel.py is not completely functional and updates are coming soon

---
+++ BotPanel.py
@@ -0,0 +1,59 @@
+# -----------------------------------------------------
+# NOTE: Currently Broken
+# -----------------------------------------------------
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import paho.mqtt
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import SECRET
+
+hostname = SECRET.HOSTNAME
+serverport = SECRET.SERVERPORT
+Broker = SECRET.BROKER
+Port = SECRET.PORT
+Topic = SECRET.TOPIC
+
+
+def pub(message):
+    client = mqtt.Client(client_id=SECRET.CLIENTUSER, transport="tcp",
+                         clean_session=False, userdata=None)
+    client.connect(Broker, port=Port, keepalive=60)
+    client.publish(topic=Topic, payload=message,
+                   qos=1, retain=False)
+    client.disconnect()
+    pass
+
+
+def close():
+    webServer.server_close()
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        file = open("BeanBotPanel.html", "r")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(str(file.read()), "utf-8"))
+
+    def do_POST(self):
+        length = int(self.headers.get('Content-length', 0))
+        data = self.rfile.read(length).decode()
+        print(data)
+        pub(data)
+        close()
+        pass
+
+
+webServer = HTTPServer((hostname, serverport), MyServer)
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostname, serverport), MyServer)
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    webServer.server_close()
--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-import sys
-from asyncio.locks import Event
 import discord
 from discord import message
 from discord import client
@@ -7,26 +5,68 @@ from discord import embeds
 from discord.enums import Status
 from discord.ext import commands
 import os
+from discord.ext.commands.core import is_owner
 from ping3 import ping
 import time
-from discord.player import FFmpegAudio
+import paho.mqtt.client as mqtt
+import SECRET
+from multiprocessing import Process
+from syncer import sync
+import asyncio
 
 intents = discord.Intents.default()
 intents.presences = True
 bot = commands.Bot(command_prefix='/', intents=intents)
-TOKEN = 'Your Token Here'
+TOKEN = SECRET.TOKEN
+Broker = SECRET.BROKER
+Port = SECRET.PORT
+Topic = SECRET.TOPIC
+
+cheese = mqtt.Client(client_id=SECRET.CLIENTUSER, transport="tcp",
+                     clean_session=False, userdata=None)
+
+cheese.connect(host=Broker, port=Port, keepalive=60)
+
+cheese.subscribe(topic=Topic, qos=1)
+
+
+@bot.event
+async def on_disconnect():
+    global bot
+    bot = commands.Bot(command_prefix='/', intents=intents)
+    inactive = discord.Game(name="Disconnecting...")
+    await bot.change_presence(activity=inactive, status=discord.Status.idle)
+    await bot.change_presence(activity=inactive, status=discord.Status.idle)
+    time.sleep(1.5)
+    await bot.change_presence(activity=None, status=discord.Status.invisible)
+    await bot.change_presence(activity=None, status=discord.Status.invisible)
+
+
+async def ifClosed():
+    if discord.Client.is_closed(bot) == True:
+        await on_disconnect()
 
 
 def Pinged(host):
-    cheese = ping(host) * 1000
+    mes = ping(host) * 1000
     # print(ping(host))
     time.sleep(1)
-    hee = str(cheese)
+    hee = str(mes)
     wee = hee[0] + hee[1] + hee[2] + hee[3] + hee[4] + hee[5] + 'ms'
     time.sleep(1)
     toReturn = str("Pinged " + host + ": " + wee)
     print(toReturn)
     return wee
+
+
+@sync
+async def on_message(client, userdata, message):
+    mes = str(message.payload.decode())
+    # print(mes)
+    if mes == "Disconn":
+        print(mes)
+        await bot.close()
+        await ifClosed()
 
 
 @bot.command()
@@ -51,10 +91,10 @@ async def AboutMe(ctx):
     welc = open("BotJoin.md", "r")
     welc = str(welc.read())
     Embed = discord.Embed(
-        title="BeanBot version 0.2", url="https://github.com/BeanMan1408/BeanBot", description=welc, color=0xffffff)
+        title="BeanBot version 0.2", url="https://github.com/BeanMan1408/BeanBot-v0.2", description=welc, color=0xffffff)
     Embed.set_thumbnail(
         url="https://i.postimg.cc/zDH5KL3W/Bean-Bot-Transparent-Back.png")
-    Embed.set_author(name="BeanMan", url="https://github.com/BeanMan1408/BeanBot-v0.1",
+    Embed.set_author(name="BeanMan", url="https://github.com/BeanMan1408/BeanBot-v0.2",
                      icon_url="https://i.postimg.cc/kgKdZLW5/Bean-Man2-132.gif")
     Embed.add_field(name="GitHub Description",
                     value="An Open Source Bot created by Me! (If you are gonna use the code to create a bot of your own. Please use a different name and bot profile picture)")
@@ -63,23 +103,24 @@ async def AboutMe(ctx):
 
 @bot.command()
 async def Disconnect(cxt):
-    user = str(cxt.author)
-    if user == "BeanMan#8008":
+    bot.owner_id = 611983174305841152
+    user = cxt.author.id
+    if user == bot.owner_id:
         await cxt.reply("Disconnecting...")
         time.sleep(1)
+        await bot.change_presence(status=discord.Status.invisible, activity=None)
+        await bot.change_presence(status=discord.Status.invisible, activity=None)
         await bot.change_presence(status=discord.Status.invisible, activity=None)
         await bot.close()
 
 
 @bot.event
 async def on_guild_join(guild):
-    #print("change Text")
-    # print(guild.text_channels)
     welc = open("BotJoin.md", "r")
     welc = str(welc.read())
     channel = bot.get_channel(839919450186711121)
     Embed = discord.Embed(
-        title="BeanBot", url="https://github.com/BeanMan1408/BeanBot-v0.1", description=welc, color=0xf6ff00)
+        title="BeanBot", url="https://github.com/BeanMan1408/BeanBot-v0.2", description=welc, color=0xf6ff00)
     Embed.set_thumbnail(
         url="https://i.postimg.cc/zDH5KL3W/Bean-Bot-Transparent-Back.png")
     Embed.set_author(name="BeanMan", url="https://github.com/BeanMan1408",
@@ -89,10 +130,35 @@ async def on_guild_join(guild):
 
 @bot.event
 async def on_ready():
-    print("w")
+    print("Online")
     active = discord.Game(name="Active")
     await bot.change_presence(activity=active, status=discord.Status.online)
 
 
-bot.run(TOKEN)
-bot.close
+def ListOfCommands():
+    global bot
+    bot = commands.Bot(command_prefix='/', intents=intents)
+    bot.add_command(PingWifi)
+    bot.add_command(Help)
+    bot.add_command(AboutMe)
+    return
+
+
+cheese.on_message = on_message
+
+
+def startBot():
+    bot.run(TOKEN)
+
+
+def startServer():
+    cheese.loop_forever()
+
+
+if __name__ == "__main__":
+    p3 = Process(target=ListOfCommands)
+    p1 = Process(target=startBot)
+    #p2 = Process(target=startServer)
+    p1.start()
+    # p2.start()
+    p3.start()
